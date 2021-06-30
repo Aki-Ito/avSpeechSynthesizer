f852569db664b3a75aea556f0f84cf8fb37658fd
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2021-06-30 22:35:31 +0900

    Initial Commit

---
+++ avSpeechSynthesizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  avSpeechSynthesizer
+//
+//  Created by 伊藤明孝 on 2021/06/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
