245f2053170d455da88fbdc425bc018b0ea9b032
Akitaka Ito <a2ki0ta0ka0@icloud.com>
2021-12-05 13:45:57 +0900

    最新版

--- avSpeechSynthesizer/ViewController.swift
+++ avSpeechSynthesizer/ViewController.swift
@@ -6,13 +6,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var speechSynthesizer: AVSpeechSynthesizer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.speechSynthesizer = AVSpeechSynthesizer()
+        //読み上げる、文字、言語などの設定
+        let utterance = AVSpeechUtterance(string: "こんにちは")//読み上げる文字
+        utterance.voice = AVSpeechSynthesisVoice(language: "ja-JP")//言語
+        utterance.rate = 0.5;  //読み上げる声のピッチ
+        utterance.preUtteranceDelay = 0.2;
+        self.speechSynthesizer.speak(utterance)
     }
+    
+    
 
 
 }
